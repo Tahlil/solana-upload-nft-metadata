94ef44c2fa4610da1b617f09a3b98d7459093a98
Tahlil <bsse0803@iit.du.ac.bd>
2024-01-22 10:10:37 +0600

    Add function to store nft json data

--- aop.js
+++ aop.js
@@ -32,6 +32,36 @@ async function storeFilesInNFTStorage(files) {
   return cid;
 }
 
+// Import the NFTStorage class and File constructor from the 'nft.storage' package
+import { NFTStorage, File } from 'nft.storage'
+
+// The 'mime' npm package helps us set the correct file type on our File objects
+import mime from 'mime'
+
+// The 'fs' builtin module on Node.js provides access to the file system
+import fs from 'fs'
+
+// The 'path' module provides helpers for manipulating filesystem paths
+import path from 'path'
+
+// Paste your NFT.Storage API key into the quotes:
+const NFT_STORAGE_KEY = 'REPLACE_ME_WITH_YOUR_KEY'
+
+async function storeNFT(imagePath, name, description) {
+    // load the file from disk
+    const image = await fileFromPath(imagePath)
+
+    // create a new NFTStorage client using our API key
+    const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
+
+    // call client.store, passing in the image & metadata
+    return nftstorage.store({
+        image,
+        name,
+        description,
+    })
+}
+
 const nftNames = [];
 
 let namesAndDescriptionsValid = validFileStructure();
